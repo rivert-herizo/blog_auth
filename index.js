ca4b7e7bf4efff090706941edf09dbe2e398b80c
Hello <totoroherizo@gmail.com>
2025-07-01 18:29:05 +0300

    add the modal for posting

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ import bodyParser from 'body-parser';
 import ejs from 'ejs';
 import expressEjsLayouts from 'express-ejs-layouts';
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Initialize Express app
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressEjsLayouts);
-app.set('view engine', 'ejs');
+// Middleware setup
+app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(expressEjsLayouts); // Use EJS layouts
+app.set('view engine', 'ejs'); // Set EJS as the templating engine
 
+
+// PostgreSQL database connection
 const db = new pg.Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
@@ -25,13 +30,15 @@ const db = new pg.Client({
 
 db.connect();
 
+// Define routes
 app.get('/home', (req, res) =>  {
     res.render('index.ejs', {
     layout: 'layout', 
     title: 'Home' 
 })
-})
+});
 
+// initialize server
 app.listen(port, () => {
     console.log("Server is running on port " + port)
-})
+});
